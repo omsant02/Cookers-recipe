864880bbd2a30ae61a20cbbac4a798e5ca969b5e
omsant02 <omsantoshwar02@gmail.com>
2023-10-01 13:58:34 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '97549774-a88e-4b07-b21b-c31af37b0a24';
+export const MODAL_CLOSE_SEC = 2.5;
